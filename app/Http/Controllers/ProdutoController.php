096d22e593c65345b8ff73760a01ab9bc9f748a4
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2020-11-25 14:27:00 -0300

    Passo 3

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Produto;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+
+class ProdutoController extends Controller
+{
+    public function pesquisar()
+    {
+        // Recebe o conteúdo elemento 'descricao' do formulário
+        $descricao = Input::get('descricao');
+        
+        // Busca produtos com o conteúdo da $descricao
+        $produtos = Produto::where('descricao', 'like', '%'.$descricao.'%')->get();
+        
+        // Chama a view produto.pesquisar e envia os produtos encontrados
+        return view('produto.pesquisar')->with('produtos', $produtos);
+    }
+}
