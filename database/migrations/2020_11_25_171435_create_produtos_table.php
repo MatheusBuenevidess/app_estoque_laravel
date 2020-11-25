1edf7e24b844038e85ae50137d6ea2058312e320
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2020-11-25 14:15:33 -0300

    Estrutura inicial

---
+++ database/migrations/2020_11_25_171435_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('descricao');
+            $table->integer('quantidade');
+            $table->decimal('valor', 15, 2);
+            $table->date('data_vencimento')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
